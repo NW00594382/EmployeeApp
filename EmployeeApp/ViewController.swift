b0418a39ebfe0682f3769a4f6eeb721ab58cb5a3
Nikhil Wagh <nikhil.g.wagh@gmail.com>
2020-05-12 19:12:23 +0530

    Initial Commit

---
+++ EmployeeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmployeeApp
+//
+//  Created by Nikhil Wagh on 5/12/20.
+//  Copyright © 2020 Tech Mahindra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
